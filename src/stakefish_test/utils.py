4db8608b770b19162b83c6eb7f89b07d1ccde873
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-13 19:52:36 +0200

    Added request and response objects

--- src/stakefish_test/utils.py
+++ src/stakefish_test/utils.py
@@ -1,4 +1,7 @@
 import importlib.metadata
+from ipaddress import AddressValueError
+from pydantic import BaseModel, Field
+from pydantic.networks import IPv4Address
 from pathlib import Path
 
 import toml
@@ -17,3 +20,19 @@ def get_version(package_name: str):
             return data["project"]["version"]
 
         return "undefined"
+
+class QueryRequest(BaseModel):
+    domain: str = Field(examples=["cloudflare.com"])
+
+class ValidateIPRequest(BaseModel):
+    ip: str = Field(examples=["192.168.5.4"])
+
+    def is_valid(self) -> bool:
+        try:
+            IPv4Address(self.ip)
+        except AddressValueError:
+            return False
+        return True
+
+class ValidateIPResponse(BaseModel):
+    valid: bool
