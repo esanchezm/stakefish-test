b8f11b88b40be8258ed5ab4b37cd4c227718b474
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-13 19:50:58 +0200

    Added get_application() and /health endpoint

--- src/stakefish_test/main.py
+++ src/stakefish_test/main.py
@@ -6,13 +6,35 @@ from fastapi import FastAPI
 
 VERSION = utils.get_version("stakefish-test")
 
-app = FastAPI()
+def get_application():
+    app = FastAPI(
+        title='Interview challenge',
+        description='Implementation for the interview challenge',
+        debug="DEBUG" in os.environ,
+        version=VERSION,
+    )
 
+
+    return app
+
+app = get_application()
 
 @app.get("/")
 def root():
+    """
+    Return basic version information
+    """
     return {
         "version": VERSION,
         "date": time.time(),
         "kubernetes": "KUBERNETES_SERVICE_HOST" in os.environ,
+    }
+
+@app.get("/health")
+def health():
+    """
+    Return health status
+    """
+    return {
+        "status": "up",
     }
