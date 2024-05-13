44f75635bc2914916230fff327de8dfb258a450f
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-13 19:51:58 +0200

    Added resolve to Query model

--- src/stakefish_test/models.py
+++ src/stakefish_test/models.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
+from typing import Dict, Optional
 from pydantic.networks import IPv4Address
+from dns import resolver, rdatatype
 
 
 class Address(BaseModel):
@@ -13,3 +15,8 @@ class Query(BaseModel):
     client_ip: str
     created_at: int
     domain: str
+
+    def resolve(self) -> list[Address]:
+        answer = resolver.resolve(qname=self.domain, rdtype=rdatatype.A)
+
+        self.addresses = [Address(id="", ip=IPv4Address(ip_addr)) for ip_addr in answer.rrset.items.keys()]
