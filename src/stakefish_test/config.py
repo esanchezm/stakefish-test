a37313ef9b035c691dfa1ba981ead1d6113a96ee
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-15 17:40:43 +0000

    Fixed deprecated use of class Config

--- src/stakefish_test/config.py
+++ src/stakefish_test/config.py
@@ -9,10 +9,7 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_HOST: str
     POSTGRES_PORT: str
-
-    class Config:
-        case_sensitive = True
-        env_file = ".env"
+    model_config = {"case_sensitive": True, "env_file": ".env"}
 
     def dsn(self):
         return f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
