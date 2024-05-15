7bd0876f635f14f3749effd95a706d84e9568ffd
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-15 06:49:18 +0000

    Added lifespan cycle and comment about health endpoint

--- src/stakefish_test/main.py
+++ src/stakefish_test/main.py
@@ -1,11 +1,19 @@
 import os
 import time
+from contextlib import asynccontextmanager
 
-import routers
-import utils
 from fastapi import FastAPI
 
+from . import database, routers, utils
+
 VERSION = utils.get_version("stakefish-test")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    database.create_db_and_tables()
+    yield
+    database.engine.dispose()
 
 
 def get_application():
@@ -14,6 +22,7 @@ def get_application():
         description="Implementation for the interview challenge",
         debug="DEBUG" in os.environ,
         version=VERSION,
+        lifespan=lifespan,
     )
 
     app.include_router(routers.tools_router)
@@ -42,6 +51,9 @@ def health():
     """
     Return health status
     """
+    # This could be improved by checking the database connection
+    # but based on my experience it's better to have it in the readiness probe
+    # Since this application has no external dependencies, it's fine to return "up"
     return {
         "status": "up",
     }
