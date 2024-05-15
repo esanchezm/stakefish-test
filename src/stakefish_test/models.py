769e34a0e9bac055c26c67cbb87e4937fa1017e2
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-15 06:53:31 +0000

    Added Base and Output classes for models

--- src/stakefish_test/models.py
+++ src/stakefish_test/models.py
@@ -1,26 +1,50 @@
+from datetime import datetime
 from typing import Optional
 
-from dns import rdatatype, resolver
-from pydantic import BaseModel
+import sqlalchemy
+from dns import exception, rdatatype, resolver
 from pydantic.networks import IPv4Address
+from sqlmodel import Field, Relationship, SQLModel
+from .utils import IPv4AddresssType
+
+class AddressBase(SQLModel):
+    ip: IPv4Address = Field(nullable=False, sa_type=IPv4AddresssType)
+
+class Address(AddressBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    query_id: int | None = Field(default=None, foreign_key="query.id")
+    query: Optional["Query"] = Relationship(back_populates="addresses") # type: ignore
 
 
-class Address(BaseModel):
-    id: Optional[str]
-    ip: IPv4Address
+class AddressOutput(AddressBase):
+    pass
 
-
-class Query(BaseModel):
-    id: str
-    addresses: list[Address]
-    client_ip: str
-    created_at: int
+class QueryBase(SQLModel):
+    client_ip: IPv4Address = Field(sa_type=IPv4AddresssType)
     domain: str
 
-    def resolve(self) -> list[Address]:
-        answer = resolver.resolve(qname=self.domain, rdtype=rdatatype.A)
+class Query(QueryBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime | None = Field(
+        default=None,
+        sa_type=sqlalchemy.DateTime(timezone=True),
+        sa_column_kwargs={"server_default": sqlalchemy.func.now()},
+        nullable=False,
+    )
+    addresses: list[Address] = Relationship(back_populates="query")
+
+    def resolve(self):
+        try:
+            answer = resolver.resolve(qname=self.domain, rdtype=rdatatype.A)
+        except exception.DNSException:
+            return []
 
         self.addresses = [
-            Address(id="", ip=IPv4Address(ip_addr))
-            for ip_addr in answer.rrset.items.keys()
+            Address(ip=IPv4Address(ip_addr)) for ip_addr in answer.rrset.items.keys()
         ]
+
+class QueryOutput(QueryBase):
+    id: int
+    domain: str
+    created_at: datetime
+    addresses: list[AddressOutput]
