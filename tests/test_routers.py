f4599d49206e48ba2e0d89bdab505647a96f40b7
Esteban Sánchez <esteban.sanchez@gmail.com>
2024-05-15 17:47:15 +0000

    Added tests for invalid input

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -23,3 +23,22 @@ def test_validate_false():
 
     assert response.status_code == 200
     assert response.json() == {"valid": False}
+
+
+def test_validate_error():
+    response = client.post(
+        "/tools/validate",
+        json={"NONE": "292.168.5.4"},
+    )
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "type": "missing",
+                "loc": ["body", "ip"],
+                "msg": "Field required",
+                "input": {"NONE": "292.168.5.4"},
+            }
+        ]
+    }
